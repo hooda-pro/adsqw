52f60a05db5d33fc200841e97854423346cfbe47
hooda-pro <krahofa2020@gmail.com>
2026-08-31 01:03:35 +0300

    Delete db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-// lib/db.js
-// اتصال قاعدة البيانات (Neon Postgres عن طريق @vercel/postgres)
-// لازم تحط DATABASE_URL أو POSTGRES_URL في Environment Variables على Vercel
-
-const { sql } = require('@vercel/postgres');
-
-module.exports = { sql };
